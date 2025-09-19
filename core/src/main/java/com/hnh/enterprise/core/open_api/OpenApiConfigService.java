e8dd6ebe1f17d80dd8691b0c2f3ff7ae06656310
huyhanhat <withuwe021@gmail.com>
2025-09-19 15:09:03 +0700

    feat: Enhance OpenAPI configuration by adding server setup and security schemes for improved API security and structure

--- core/src/main/java/com/hnh/enterprise/core/open_api/OpenApiConfigService.java
+++ core/src/main/java/com/hnh/enterprise/core/open_api/OpenApiConfigService.java
@@ -1,16 +1,41 @@
 package com.hnh.enterprise.core.open_api;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
 
 public record OpenApiConfigService(OpenApiProperties openApiProperties) {
     
     public OpenAPI openAPI() {
-        if (openApiProperties.getServers() == null) {
-            return new OpenAPI();
+        OpenAPI openAPI = new OpenAPI();
+        if (openApiProperties.getServers() != null) {
+            setupServers(openAPI, openApiProperties.getServers());
         }
-        return new OpenAPI().servers(
-                openApiProperties.getServers().stream().map(serverUrl -> new Server().url(serverUrl)).toList()
-        );
+        
+        setupSecuritySchemes(openAPI);
+        
+        return openAPI;
+    }
+    
+    private void setupServers(OpenAPI openAPI, List<String> servers) {
+        List<Server> serverList = servers.stream().map(serverUrl -> new Server().url(serverUrl)).toList();
+        openAPI.servers(serverList);
+    }
+    
+    private void setupSecuritySchemes(OpenAPI openAPI) {
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
+                                .bearerFormat("JWT"))
+                .addSecuritySchemes("apiKey",
+                        new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER)
+                                .name("X-API-Key"));
+        openAPI.components(components);
+        
+        openAPI.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
